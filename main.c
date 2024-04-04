6be84a728bc23c9701e8906dfabcf178b1922ba1
excf28 <deee4444@gmail.com>
2024-04-04 19:49:08 +0200

    fixes

--- main.c
+++ main.c
@@ -2,14 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <time.h>
 #include <string.h>
 #include <semaphore.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 #define MAX_ROUNDS 20
 #define MAX_SCORE 501
@@ -17,9 +14,9 @@
 #define BASE 0
 #define OPEN_PLAYERONE 1
 #define OPEN_PLAYERTWO 2
-#define CLOSED -1
+#define CLOSED (-1)
 
-sem_t *startSem, *player1Sem, *player2Sem;
+sem_t *startSem[2], *sendSem[2], *sentSem[2];
 int pipefd[2];
 int player1Score = 0, player2Score = 0;
 int player1Hits[7] = {0}, player2Hits[7] = {0};
@@ -40,77 +37,82 @@ struct Throw throwDart() {
     int area = rand() % 9 + 13;
     int multiplier = rand() % 3 + 1;
     int score = area;
-    area -= 15;
 
     if (score == 21) {
         if (multiplier == 3) score = 50;
         else score = 25;
     } else score = area * multiplier;
 
+    area -= 15;
+
     struct Throw throw = {area, multiplier, score};
     return throw;
 }
 
-void player(int playerID) {
-    // TODO: pause for signal here
-    char name[20];
-    sprintf(name, "Player %d", playerID);
-    printf("%s has joined the game.\n", name);
-    srand(time(NULL) * playerID);
+void signalHandler(int signal) {
+    printf("Signal %d received.\n", signal);
+}
 
-    while (1) {
-        // TODO: pause for signal here
-        sem_wait(startSem);
+void player(int playerID) {
+    char name[20];
+    sprintf(name, "Player %d", playerID + 1);
+    printf("%s has joined the game.\n", name);
+    srand(time(NULL) * (playerID + 1));
+
+    while (true) {
+        sem_wait(startSem[playerID]);
         delay();
         struct Throw throw = throwDart();
-        printf("%s has thrown %d * %d.\n", name, throw.area, throw.multiplier);
+        printf("%s has thrown %d * %d.\n", name, throw.area + 15, throw.multiplier);
 
-        if (playerID == 1) {
-            sem_wait(player1Sem);
-            write(pipefd[1], &throw, sizeof(struct Throw));
-            sem_post(player1Sem);
-        } else {
-            sem_wait(player2Sem);
-            write(pipefd[1], &throw, sizeof(struct Throw));
-            sem_post(player2Sem);
-        }
+        sem_wait(sendSem[playerID]);
+        write(pipefd[1], &throw, sizeof(struct Throw));
+        sem_post(sentSem[playerID]);
     }
 }
 
 int main() {
-    sem_unlink("/startSem");
-    sem_unlink("/player1Sem");
-    sem_unlink("/player2Sem");
-    startSem = sem_open("/startSem", O_CREAT, 0644, 0);
-    player1Sem = sem_open("/player1Sem", O_CREAT, 0644, 0);
-    player2Sem = sem_open("/player2Sem", O_CREAT, 0644, 0);
+    startSem[0] = sem_open("/startSem0", O_CREAT, S_IRUSR | S_IWUSR, 0);
+    startSem[1] = sem_open("/startSem1", O_CREAT, S_IRUSR | S_IWUSR, 0);
+    sendSem[0] = sem_open("/sendSem0", O_CREAT, S_IRUSR | S_IWUSR, 0);
+    sendSem[1] = sem_open("/sendSem1", O_CREAT, S_IRUSR | S_IWUSR, 0);
+    sentSem[0] = sem_open("/sentSem0", O_CREAT, S_IRUSR | S_IWUSR, 0);
+    sentSem[1] = sem_open("/sentSem1", O_CREAT, S_IRUSR | S_IWUSR, 0);
 
     pipe(pipefd);
 
     pid_t player1PID = fork();
     if (player1PID == 0) {
-        player(1);
-        exit(0);
+        signal(SIGUSR1, signalHandler);
+        pause();
+        player(0);
+        return 0;
     }
 
     pid_t player2PID = fork();
     if (player2PID == 0) {
-        player(2);
-        exit(0);
+        signal(SIGUSR1, signalHandler);
+        pause();
+        player(1);
+        return 0;
     }
+
+    kill(player1PID, SIGUSR1);
+    sleep(1);
+    kill(player2PID, SIGUSR1);
+    sleep(1);
 
     int round = 1;
     while (round <= MAX_ROUNDS && player1Score < MAX_SCORE && player2Score < MAX_SCORE) {
-        printf("Round %d\n", round);
-        sem_post(startSem);
-        sem_post(startSem);
-
+        printf("\nRound %d\n", round);
         struct Throw throw1, throw2;
 
-        // TODO: fix this cursed semaphore
-        sem_post(player1Sem);
+        sem_post(startSem[0]);
+        sem_post(startSem[1]);
+
+        sem_post(sendSem[0]);
+        sem_wait(sentSem[0]);
         read(pipefd[0], &throw1, sizeof(struct Throw));
-        sem_wait(player1Sem);
 
         if (throw1.area > 0 && areaStates[throw1.area] != CLOSED) {
             player1Hits[throw1.area] += throw1.multiplier;
@@ -127,10 +129,9 @@ int main() {
             }
         }
 
-        // TODO: fix this cursed semaphore
-        sem_post(player2Sem);
+        sem_post(sendSem[1]);
+        sem_wait(sentSem[1]);
         read(pipefd[0], &throw2, sizeof(struct Throw));
-        sem_wait(player2Sem);
 
         if (throw2.area > 0 && areaStates[throw2.area] != CLOSED) {
             player2Hits[throw2.area] += throw2.multiplier;
@@ -168,6 +169,9 @@ int main() {
 
     kill(player1PID, SIGKILL);
     kill(player2PID, SIGKILL);
+
+    close(pipefd[0]);
+    close(pipefd[1]);
 
     if (player1Score >= player2Score) {
         printf("Player 1 wins!\n");
